31c28ae7bd8bd191bd951c78ac893557cae34c71
pablolitaratus <pablogeminis1978@gmail.com>
2015-10-08 21:18:31 +0200

    Initial commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,31 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.PushPlugin.server/www/PushNotification.js",
+        "id": "com.PushPlugin.server.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.PushPlugin.server": "1.0.0",
+    "cordova-plugin-device": "1.0.1"
+}
+// BOTTOM OF METADATA
+});
